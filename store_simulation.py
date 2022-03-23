48da4ed93c3d553efa2b72764579adc78020231c
Eduardo N Gomes <Eduardo.000@live.com>
2022-03-23 17:09:40 -0300

    Apenas um arquivo que para simular um programa de uma loja usando tuple

---
+++ store_simulation.py
@@ -0,0 +1,33 @@
+t1 = ('Doce', 2.3, 'Bala', 0.15, 'Pizza', 28.9, 'Arroz', 4.5, 'Paçoca', 0.5, 'Pamonha', 5.4)
+soma = 0
+
+print('-'*30)
+print()
+print('-'*9, 'CARDAPIO', '-'*11)
+print()
+print('-'*30)
+
+for v,i in enumerate(t1):
+    if v % 2 == 0:
+        print(f'{i:<7}',end=' ')
+        print('.........',end=' ')
+    else:
+        print(f'R$: {i:>}')
+print('-'*30)
+
+while True:
+    user_shop = str(input('Qual item voce deseja comprar? ')).capitalize()
+    item = t1.index(user_shop)+1
+    soma += t1[item]
+    
+
+    while True:
+        user_answer = str(input('Deseja continuar? ')).strip()[0]
+        if user_answer in 'SsNn':
+            break
+        elif user_answer not in 'SsNn':
+            print('Opcao invalida')
+    if user_answer in 'Nn':
+        break
+
+print(f'A soma total dos objetos comprados foi de: R$ {soma:>.2f}.')
